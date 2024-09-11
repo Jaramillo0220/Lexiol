fff31b26c802f8a9de5221bb274d746a5ce9cd19
Jaramillo0220 <joseangel0220@outlook.es>
2024-09-11 15:16:55 -0600

    Se agrego el contador de linea para prueba

--- obj/Debug/net8.0/Lexico1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lexico1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lexico1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef099a29208577f1359d4c26790ff2d61c5684ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab97d85546f138a0a443e0a94d64b2ed916b4bf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lexico1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lexico1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
